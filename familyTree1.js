8a9d3029fffa366643ecec2b05e54cc41afcbd47
nguyenduong1579 <84682346+nguyenduong1579@users.noreply.github.com>
2021-10-13 09:36:21 +0100

    Update familyTree1.js

--- familyTree1.js
+++ familyTree1.js
@@ -2,19 +2,19 @@ var firstgen = [
     {
         name: "King Georger VI",
         paren: ["Price Phillip","Queen Elizabeth II","Prince Margaret"],
-        ChildOf: "",
-        childOf: function (){ 
-            return this.childof;
-        }
+        childof: [""],
+        childOf: function () {
+            return this.childof.map(parent => firstgen[0].childof).join(' & ') || "unknown"
+          }
     },
 
     {
         name: "Queen Elizabeth",
         paren: ["Price Phillip","Queen Elizabeth II","Prince Margaret"],
-        childOf: "",
-        childOf: function (){ 
-            return this.childof;
-        }
+        childof: [""],
+        childOf: function () {
+            return this.childof.map(parent => firstgen[1].childof).join(' & ') || "unknown"
+          }
     }
 ]
 
@@ -23,17 +23,17 @@ var secondgen = [
         name: "Prince Philip",
         parent: [" "],
         childof: ["King Georger VI","Queen Elizabeth"],
-        childOf: function (){ 
-            return this.childof;
-        }
+        childOf: function () {
+            return this.childof.map(parent => secondgen[0].childof).join(' & ') || "unknown"
+          }
     },
     {
         name: "Queen Elizabeth II",
         parent: [" "],
         childof: ["King Georger VI","Queen Elizabeth"],
-        childOf: function (){ 
-            return this.childof;
-        }
+        childOf: function () {
+            return this.childof.map(parent => secondgen[1].childof).join(' & ') || "unknown"
+          }
     }
 ]
 
@@ -41,50 +41,50 @@ var thirdgen = [
     {
         name: "camila",
         parent: [" "],
-        childof: ["Prince Philip","Queen Elizabeth II"],
-        childOf: function (){ 
-            return this.childof;
-        }
+        childof: ["Prince Philip & Queen Elizabeth II"],
+        childOf: function () {
+            return this.childof.map(parent => thirdgen[0].childof).join(' & ') || "unknown"
+          }
     },
     {
         name: "Charles",
         parent: [" "],
-        childof: ["Prince Philip","Queen Elizabeth II"],
-        childOf: function (){ 
-            return this.childof;
-        }
+        childof: ["Prince Philip & Queen Elizabeth II"],
+        childOf: function () {
+            return this.childof.map(parent => thirdgen[1].childof).join(' & ') || "unknown"
+          },
     },
     {
         name: "Diana",
         parent: [" "],
-        childof: ["Prince Philip","Queen Elizabeth II"],
-        childOf: function (){ 
-            return this.childof;
-        }
+        childof: ["Prince Philip & Queen Elizabeth II"],
+        childOf: function () {
+            return this.childof.map(parent => thirdgen[2].childof).join(' & ') || "unknown"
+          }
     },
     {
         name: "Anne",
         parent: [" "],
-        childof: ["Prince Philip","Queen Elizabeth II"],
-        childOf: function (){ 
-            return this.childof;
-        }
+        childof: ["Prince Philip & Queen Elizabeth II"],
+        childOf: function () {
+            return this.childof.map(parent => thirdgen[3].childof).join(' & ') || "unknown"
+          }
     },
     {
         name: "Prince Andrew",
         parent: [" "],
-        childof: ["Prince Philip","Queen Elizabeth II"],
-        childOf: function (){ 
-            return this.childof;
-        }
+        childof: ["Prince Philip & Queen Elizabeth II"],
+        childOf: function () {
+            return this.childof.map(parent => thirdgen[4].childof).join(' & ') || "unknown"
+          }
     },
     {
         name: "Prince Edward",
         parent: [" "],
-        childof: ["Prince Philip","Queen Elizabeth II"],
-        childOf: function (){ 
-            return this.childof;
-        }
+        childof: ["Prince Philip & Queen Elizabeth II"],
+        childOf: function () {
+            return this.childof.map(parent => thirdgen[5].childof).join(' & ') || "unknown"
+          },
     },
 ]
-  console.log(thirdgen[0].childOf())
+  console.log(firstgen[0].childOf())
